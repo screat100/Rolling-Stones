3ec2b1a68aaeb928c996083fbd880fae8537cf75
screat100 <61044260+screat100@users.noreply.github.com>
2020-10-02 23:08:33 +0900

    Main Scene 추가

---
+++ RollingStones/Assets/mainScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainScene : MonoBehaviour
+{
+    public void GameStart()
+    {
+        SceneManager.LoadScene("Stage1");
+    }
+
+    void Shop()
+    {
+
+    }
+
+    void Option()
+    {
+
+    }
+
+    void Exit()
+    {
+
+    }
+
+
+}
