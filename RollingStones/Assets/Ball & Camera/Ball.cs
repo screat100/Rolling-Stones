eb3b04f7b8bde3d063717c96e1ef022b3945262a
screat100 <61044260+screat100@users.noreply.github.com>
2020-10-13 12:35:54 +0900

    i realize how to apply 'revert' to commit 5

--- RollingStones/Assets/Ball & Camera/Ball.cs
+++ RollingStones/Assets/Ball & Camera/Ball.cs
@@ -49,17 +49,10 @@ public class Ball : MonoBehaviour
             // 속도비례 데미지 주기!
             other.gameObject.GetComponent<DoorStat>().DoorTakeDamage(damage);
 
-            SoundManager.Instance.PlayDoorSound(); //성문 부딪혔을 때 재생
-
             // 시작지점으로 복귀
             gameObject.transform.position = startPos;
             rb.velocity = new Vector3(0, 0, 0);
             rb.angularVelocity = new Vector3(0, 0, 0);
-        }
-
-        if (other.gameObject.tag == "wall")
-        {
-            SoundManager.Instance.PlayWallSound();
         }
 
 
@@ -79,7 +72,6 @@ public class Ball : MonoBehaviour
         // 추락지역에 떨어지면 최초 시작지점으로 이동
         if (other.gameObject.tag == "fall")
         {
-            SoundManager.Instance.PlayFallSound();
             Debug.Log("Falled !");
             gameObject.transform.position = startPos;
             rb.velocity = new Vector3(0, 0, 0);
@@ -95,8 +87,6 @@ public class Ball : MonoBehaviour
             //rb.AddForce(new Vector3(0, 1, 0) * 400);
             rb.velocity += new Vector3(0, 5, 0);
             canJump = false;
-
-            SoundManager.Instance.PlayJumpSound(); //점프할 때 재생
         }
 
         //speed = rb.velocity.z;
