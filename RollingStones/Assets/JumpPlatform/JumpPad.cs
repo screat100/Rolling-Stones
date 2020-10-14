10f11cab47350e78a05ac464515904d088904595
hoo-o <72002468+hoo-o@users.noreply.github.com>
2020-10-14 22:45:17 +0900

    JumpPad 추가
    점프발판 추가아

---
+++ RollingStones/Assets/JumpPlatform/JumpPad.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPad : MonoBehaviour
+{
+    public float move = 20f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+
+            Rigidbody Ball = other.gameObject.GetComponent<Rigidbody>();
+            Ball.velocity += new Vector3(0, transform.up.y, 0) * move;
+        }
+    }
+}
