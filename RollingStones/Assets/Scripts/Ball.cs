820d40fbd449b34f000e165a2dc717fe91caa85a
hjoannejiyeon <hjoanne@naver.com>
2020-10-21 00:07:28 +0900

    추락 시(완료)
    추락 시 화면 어둡게->시작 화면 다시 밝아짐

--- RollingStones/Assets/Scripts/Ball.cs
+++ RollingStones/Assets/Scripts/Ball.cs
@@ -100,6 +100,8 @@ public class Ball : MonoBehaviour
 
             Invoke("FallingDead", 2);
             SoundManager.Instance.PlayFallSound();
+            fadeinout.Fadeinout.FadeInImage();
+            //fadeinout.Fadeinout.FadeOutImage();
         }
     }
 
@@ -140,6 +142,7 @@ public class Ball : MonoBehaviour
         if (!FindObjectOfType<ui_manager>().isStageOver)
         {
             neverMove = false;
+            fadeinout.Fadeinout.FadeOutImage();
             gameObject.transform.position = startPos;
             rb.velocity = new Vector3(0, 0, 0);
             rb.angularVelocity = new Vector3(0, 0, 0);
--- RollingStones/Assets/Scripts/fadeinout.cs
+++ RollingStones/Assets/Scripts/fadeinout.cs
@@ -7,17 +7,21 @@ public class fadeinout : MonoBehaviour
 {
     public static fadeinout Fadeinout;
     public Image Fade;
-    public float FadeTime = 2f;
-    float start;
-    float end;
-    float time = 0f;
+    public float FadeInTime = 0.5f;
+    public float FadeOutTime = 0.5f;
     bool isPlaying = false;
-    
+
+    void Awake()
+    {
+        if (fadeinout.Fadeinout == null)
+            fadeinout.Fadeinout = this;
+    }
+
     void Sart()
     {
-
+        Fade = GetComponent<Image>();
     }
-    public void FadeInImage()
+    public void FadeInImage()//불투명
     {
         if (isPlaying == true) //중복재생방지
         {
@@ -26,7 +30,7 @@ public class fadeinout : MonoBehaviour
         StartCoroutine("fadeinplay");    //코루틴 실행
     }
 
-    public void FadeOutImage()
+    public void FadeOutImage()//투명
     {
         if (isPlaying == true) //중복재생방지
         {
@@ -37,13 +41,12 @@ public class fadeinout : MonoBehaviour
 
     IEnumerator fadeinplay()
     {
-        Fade = GetComponent<Image>();
         isPlaying = true;
         Color fadecolor = Fade.color;
-        while (fadecolor.a < 1f)//갈수록 투명
+        while (fadecolor.a < 1f)//갈수록 불투명
 
         {
-            fadecolor.a += Time.deltaTime / FadeTime;
+            fadecolor.a += Time.deltaTime / FadeInTime;
             Fade.color = fadecolor;
             yield return null;
         }
@@ -52,13 +55,11 @@ public class fadeinout : MonoBehaviour
     }
     IEnumerator fadeoutplay()
     {
-        Fade = GetComponent<Image>();
         isPlaying = true;
         Color fadecolor = Fade.color;
         while (fadecolor.a > 0f)//갈수록 투명
-
         {
-            fadecolor.a += Time.deltaTime / FadeTime;
+            fadecolor.a -= Time.deltaTime / FadeOutTime;
             Fade.color = fadecolor;
             yield return null;
         }
