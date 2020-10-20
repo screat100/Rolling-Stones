afbecbbf89b28e584fc165c0831d790c8b89df5a
hjoannejiyeon <hjoanne@naver.com>
2020-10-21 00:58:20 +0900

    아주 조금 수정
    프로펠러 수정
    stage4 하는중

--- RollingStones/Assets/Scripts/propeller.cs
+++ RollingStones/Assets/Scripts/propeller.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class propeller : MonoBehaviour
-{
-    //float Speed = 50.0f;
+{ 
     // Start is called before the first frame update
     void Start()
     {
@@ -18,10 +17,10 @@ public class propeller : MonoBehaviour
     }
     void OnTriggerEnter(Collider other)
     {
-        //if (other.gameObject.name == "Player")
-        //{
-        //    Rigidbody ballRG = other.gameObject.GetComponent<Rigidbody>();
-        //    ballRG.velocity += new Vector3(transform.forward.x * Speed, 0, transform.forward.z * Speed);
-        //}
+        if (other.gameObject.name == "Player")
+        {
+            Rigidbody ballRG = other.gameObject.GetComponent<Rigidbody>();
+            ballRG.velocity += new Vector3(transform.forward.x + gameObject.transform.position.x, 0, transform.forward.z + gameObject.transform.position.z);
+        }
     }
 }
